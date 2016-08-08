a9f2a1561153087244119fc407f719cac0d8c1aa
Robert Spencer <rob@robspencer.me.uk>
2016-08-08 16:28:35 +0100

    add status endpoint

--- databox/notifier/api.py
+++ databox/notifier/api.py
@@ -5,7 +5,10 @@ from notification_services.sms import Sms
 from notification_services.push import Push
 from notification_services.growl import Growl
 from notification_services.gmail import Gmail
+from status import Status
 from notifier import app
+import os
+from pymacaroons import Macaroon, Verifier
 
 api = Api(app)
 
@@ -19,3 +22,4 @@ api.add_resource(Growl, "/notify/growl")
 api.add_resource(Push, "/notify/push")
 api.add_resource(Sms, "/notify/sms")
 api.add_resource(Twitter, "/notify/twitter")
+api.add_resource(Status, "/status")
